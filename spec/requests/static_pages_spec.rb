9db906f0acdb0ab27ebc8d122e0b0e122f7572ec
Rockefeller <rockefeller.zq@gmail.com>
2012-06-25 14:50:32 +0800

    Finish static pages.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,12 +4,45 @@ describe "StaticPages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_home_path
-      response.status.should be(200)
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the title 'Home'" do
+    visit '/static_pages/home'
+    page.should have_selector('title',
+                     :text => "Ruby on Rails tutorail SampleApp | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+    visit '/static_pages/help'
+    page.should have_selector('h1', :text => 'Help')
+    end
+    
+    it "shoule have the title 'Help'" do
+    visit '/static_pages/help'
+    page.should have_selector('title', 
+                   :text => "Ruby on Rails tutorail SampleApp | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About us')
+    end
+    
+    it "shoule have the title 'About us'" do
+    visit '/static_pages/about'
+    page.should have_selector('title',
+               :text => "Ruby on Rails tutorail SampleApp | About us")
     end
   end
 end
+
+
