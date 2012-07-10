1598c61eaf9ce6c51575c69fb9f0c2d087a54685
Rockefeller <rockefeller.zq@gmail.com>
2012-07-10 11:41:33 +0800

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Rockefeller"
+    email    "rockefeller.zq@gmail.com"
+    password "44585194"
+    password_confirmation "44585194"
+  end
+end
--- spec/requests/users_pages_spec.rb
+++ spec/requests/users_pages_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-
 describe "UsersPages" do
   subject{ page }
   shared_examples_for "all users_pages" do
@@ -14,4 +13,40 @@ describe "UsersPages" do
   
     it_should_behave_like "all users_pages"
   end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1', text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+
+  describe "signup" do
+  
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",     with: "Rockefeller"
+        fill_in "Email", with: "rockefeller.zq@gmail.com"
+        fill_in "Password", with: "44585194"
+        fill_in "Confirmation", with: "44585194"
+      end
+  
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
+
+
+
